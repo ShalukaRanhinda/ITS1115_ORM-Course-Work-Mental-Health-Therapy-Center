2c71948521cc574942ae50a51f5dcd651bfb53b5
Shaluka <shalukaranhinda@gmail.com>
2026-05-20 13:03:48 +0530

    Initial commit

---
+++ ORM-Course-Work-Mental-Health-Therapy-Center/Mental-Health-Therapy-Center-main/src/main/java/lk/ijse/therapycenter/Initializer.java
@@ -0,0 +1,24 @@
+package lk.ijse.therapycenter;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import lk.ijse.therapycenter.entity.*;
+
+import java.io.IOException;
+
+public class Initializer extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent load = FXMLLoader.load((getClass().getResource("/view/LogIn.fxml")));
+        stage.setScene(new Scene(load));
+        stage.setTitle("Center");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
